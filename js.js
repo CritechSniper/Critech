33bf9c72de5f012aa54835102d5984e1deea2b3a
Waleed Ammar <waleed.gamer173@gmail.com>
2025-02-18 11:14:48 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
 console.log(`STOP! YOU ARE NOT ALLOWED TO VIEW THE SOURCE CODE!\nIF YOU WANT TO SEE THE SOURCE CODE, PLEASE CONTACT THE OWNER OF THE WEBSITE!`);
-
 function search() {
     console.log("Search button clicked");
     var input = document.getElementById("searchbar").value;
