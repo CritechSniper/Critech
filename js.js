804c035deaf41e102dd3a99a6510577f370e92b1
Waleed Ammar <waleed.gamer173@gmail.com>
2025-02-18 11:04:58 +0400

    d
    d

--- js.js
+++ js.js
@@ -1,1 +1,6 @@
 console.log(`STOP! YOU ARE NOT ALLOWED TO VIEW THE SOURCE CODE!\nIF YOU WANT TO SEE THE SOURCE CODE, PLEASE CONTACT THE OWNER OF THE WEBSITE!`);
+function search() {
+    var input = document.getElementById("searchbar").value;
+    var url = "https://www.google.com/search?q=" + encodeURIComponent(input);
+    window.open(url, "_blank");
+}
