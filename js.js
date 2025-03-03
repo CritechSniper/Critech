8c6f8ee2a969d12861b4386eface2e5dc6a4104b
Waleed Ammar <waleed.gamer173@gmail.com>
2025-03-03 22:09:41 +0400

    Added links and hover effects

--- js.js
+++ js.js
@@ -5,3 +5,27 @@ function search() {
     var url = "https://www.google.com/search?q=" + encodeURIComponent(input);
     window.open(url, "_blank");
 }
+
+function classroomlink() {
+    console.log("Classroom button clicked");
+    var url = "https://classroom.google.com/";
+    window.open(url, "_blank");
+}
+
+function gmaillink() {
+    console.log("Gmail button clicked");
+    var url = "https://mail.google.com/";
+    window.open(url, "_blank");
+}
+
+function drivelink() {
+    console.log("Drive button clicked");
+    var url = "https://drive.google.com/";
+    window.open(url, "_blank");
+}
+
+function calendarlink() {
+    console.log("Calendar button clicked");
+    var url = "https://calendar.google.com/";
+    window.open(url, "_blank");
+}
